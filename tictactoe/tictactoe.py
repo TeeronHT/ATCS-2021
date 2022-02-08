8e1a9f37ecac48e0d0c6a7a752dba93ed459bb25
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-02-08 09:41:32 -0800

    User vs. Random AI

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -15,7 +15,7 @@ class TicTacToe:
 
     def print_instructions(self):
         # TODO: Print the instructions to the game
-        print("Welcome to Tic Tac Toe. \nPlayer 1 is X and Player 2 is 0. \nTake turns placing your pieces. The first player to get 3 in a row wins.")
+        print("Welcome to Tic Tac Toe. \nPlayer 1 is X and Player 2 is O. \nTake turns placing your pieces. The first player to get 3 in a row wins.")
 
     def print_board(self):
         # TODO: Print the board
@@ -25,7 +25,6 @@ class TicTacToe:
             for col in range(len(self.board[0])):
                 row_string += self.board[row][col] + "\t"
             print(row_string)
-
 
     def is_valid_move(self, row, col):
         # TODO: Check if the move is valid
@@ -40,12 +39,10 @@ class TicTacToe:
 
     def take_manual_turn(self, player):
         # TODO: Ask the user for a row, col until a valid response
-        # is given them place the player's icon in the right spot
+        #  is given them place the player's icon in the right spot
         input_row = int(input("Enter a row: "))
         input_col = int(input("Enter a col: "))
 
-    def take_turn(self, player):
-        # TODO: Simply call the take_manual_turn function
         while not self.is_valid_move(input_row, input_col):
             print("Please enter a valid move.")
             input_row = int(input("Enter a row: "))
@@ -54,10 +51,24 @@ class TicTacToe:
         self.place_player(player, input_row, input_col)
         self.print_board()
 
+    def take_random_turn(self, player):
+        rand_row = random.randint(0, 2)
+        rand_col = random.randint(0, 2)
+        while not self.is_valid_move(rand_row, rand_col):
+            rand_row = random.randint(0, 2)
+            rand_col = random.randint(0, 2)
+        print("Enter a row: " + str(rand_row))
+        print("Enter a col: " + str(rand_col))
+        self.place_player(player, rand_row, rand_col)
+        self.print_board()
+
     def take_turn(self, player):
         # TODO: Simply call the take_manual_turn function
         print(str(player) + "'s Turn")
-        self.take_manual_turn(player)
+        if player == "X":
+            self.take_manual_turn(player)
+        else:
+            self.take_random_turn(player)
 
     def check_col_win(self, player):
         # TODO: Check col win
