78ec6494c2fe1062952713cc59e93fc0c88919c4
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-02-17 00:47:00 -0800

    Unbeatable AI implemented

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -6,6 +6,8 @@ A program allowing two players to play tic tac toe against each other.
 Base of a later program where the user will play against an AI.
 
 """
+
+# I hope you have/had (based on when you review this) a very restful Ski Week!
 import random
 
 class TicTacToe:
@@ -49,7 +51,6 @@ class TicTacToe:
             input_col = int(input("Enter a col: "))
 
         self.place_player(player, input_row, input_col)
-        self.print_board()
 
     def take_random_turn(self, player):
         rand_row = random.randint(0, 2)
@@ -60,7 +61,6 @@ class TicTacToe:
         print("Enter a row: " + str(rand_row))
         print("Enter a col: " + str(rand_col))
         self.place_player(player, rand_row, rand_col)
-        self.print_board()
 
     def take_turn(self, player):
         # TODO: Simply call the take_manual_turn function
@@ -68,7 +68,76 @@ class TicTacToe:
         if player == "X":
             self.take_manual_turn(player)
         else:
-            self.take_random_turn(player)
+            self.take_minimax_turn(player)
+
+    def take_minimax_turn(self, player):
+        score, row, col = self.minimax(player)
+        self.place_player(player, row, col)
+
+    # Used you pseudo-code to write the minimax function
+    # I was writing a different version of it, without using you
+    # structure as much but I'm sick so I couldn't really debug it
+    # and just decided to take the easy route. Sorry
+    def minimax(self, player):
+        if (self.check_win("O")):
+            return 10, None, None
+        elif (self.check_win("X")):
+            return -10, None, None
+        elif(self.check_tie()):
+            return 0, None, None
+
+        opt_row, opt_col = -1, -1
+        if player == "O":
+            best = -10
+            for row in range(len(self.board)):
+                for col in range(len(self.board[0])):
+                    # My (fruitless) attempt at writing all the innards of these
+                    # nested for loops in one line.
+                    # [self.place_player(player, row, col); score = self.minimax("X")[0]; self.place_player("-", row, col) if self.board[row][col] == "-" else ( best, opt_row, opt_col = score, row, col if best < score )]
+                    if self.board[row][col] == "-":
+                        self.place_player(player, row, col)
+                        score = self.minimax("X")[0]
+                        self.place_player("-", row, col)
+                        if best < score:
+                            best, opt_row, opt_col = score, row, col
+            return best, opt_row, opt_col
+
+        if player == "X":
+            worst = 10
+            for row in range(len(self.board)):
+                for col in range(len(self.board[0])):
+                    if self.board[row][col] == "-":
+                        self.place_player(player, row, col)
+                        score = self.minimax("O")[0]
+                        self.place_player("-", row, col)
+                        if worst > score:
+                            worst, opt_row, opt_col = score, row, col
+            return worst, opt_row, opt_col
+
+        '''
+        # alternative method of solving
+        # I'm too sick to finish this version right now and debug
+        # I'll come back to this some point soon because I think
+        # It may be a more efficient method of solving this problem
+        # If you have any idea what the error could be please let me
+        # know lol
+        best, row, col = 1000, None, None
+        worst, row, col = -1000, None, None
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                if self.is_valid_move(i, j):
+                    self.place_player(player, i, j)
+                    if (player == "O"):
+                        score, r, c = self.minimax("X")
+                    elif (player == "X"):
+                        score, r, c = self.minimax("O")
+                    if (score > best):
+                        best, row, col = score, i, j
+                    elif (score > worst):
+                        worst, row, col = score, i, j
+                    self.place_player("-", i, j)
+        return score, row, col
+        '''
 
     def check_col_win(self, player):
         # TODO: Check col win
@@ -129,3 +198,4 @@ class TicTacToe:
                     player = "O"
                 else:
                     player = "X"
+            self.print_board()
