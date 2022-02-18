9a59d4cb5b1f9803d7b8affaf2328a6e1bd5429c
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-02-17 22:02:37 -0800

    Completion of tictactoe program

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,26 +1,33 @@
 """
 Teeron Hajebi Tabrizi
-February 7th, 2022
+February 18th, 2022
 
 A program allowing two players to play tic tac toe against each other.
-Base of a later program where the user will play against an AI.
+Base of a later program where the user will play against AIs of varying
+strength.
 
 """
 
 # I hope you have/had (based on when you review this) a very restful Ski Week!
+# I changed the arguments for the take_turn method
 import random
+
 
 class TicTacToe:
     def __init__(self):
-        # TODO: Set up the board to be '-'
+        # Set up the board to be '-'
         self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 
+    # Prints the instructions of the game and asks the player if they want to play
+    # against a friend or the AI.
     def print_instructions(self):
-        # TODO: Print the instructions to the game
+        # Print the instructions to the game
         print("Welcome to Tic Tac Toe. \nPlayer 1 is X and Player 2 is O. \nTake turns placing your pieces. The first player to get 3 in a row wins.")
+        return input("Would you like to play against a friend or the computer? (Enter F or C): ")
 
+    # Prints the board. Used when game is updates as well.
     def print_board(self):
-        # TODO: Print the board
+        # Print the board
         print("\t0\t1\t2")
         for row in range(len(self.board)):
             row_string = str(row) + "\t"
@@ -28,62 +35,73 @@ class TicTacToe:
                 row_string += self.board[row][col] + "\t"
             print(row_string)
 
+    # Checks if a move made by the user or AI is valid.
     def is_valid_move(self, row, col):
-        # TODO: Check if the move is valid
-        if 0 <= row <= len(self.board) - 1 and 0 <= col <= len(self.board[0]) - 1:
-            if self.board[row][col] == "-":
+        # Check if the move is valid
+        if (0 <= int(row) <= len(self.board) - 1 and 0 <= int(col) <= len(self.board[0]) - 1):
+            if (self.board[int(row)][int(col)] == "-"):
                 return True
         return False
 
+    # Places the player in the given row and column.
     def place_player(self, player, row, col):
-        # TODO: Place the player on the board
+        # Place the player on the board
         self.board[row][col] = player
 
+    # Called when the user(s) wants to play the game.
     def take_manual_turn(self, player):
-        # TODO: Ask the user for a row, col until a valid response
-        #  is given them place the player's icon in the right spot
-        input_row = int(input("Enter a row: "))
-        input_col = int(input("Enter a col: "))
+        # Ask the user for a row, col until a valid response
+        # is given them place the player's icon in the right spot
 
-        while not self.is_valid_move(input_row, input_col):
+        input_row = input("Enter a row: ")
+        input_col = input("Enter a col: ")
+
+        while ((input_row == '' or input_col == '') or (self.is_valid_move(input_row, input_col) is False)):
             print("Please enter a valid move.")
-            input_row = int(input("Enter a row: "))
-            input_col = int(input("Enter a col: "))
+            input_row = input("Enter a row: ")
+            input_col = input("Enter a col: ")
 
-        self.place_player(player, input_row, input_col)
+        self.place_player(player, int(input_row), int(input_col))
 
+    # Weaker version of AI that does not apply logic to solve the problem.
     def take_random_turn(self, player):
         rand_row = random.randint(0, 2)
         rand_col = random.randint(0, 2)
+
         while not self.is_valid_move(rand_row, rand_col):
             rand_row = random.randint(0, 2)
             rand_col = random.randint(0, 2)
-        print("Enter a row: " + str(rand_row))
-        print("Enter a col: " + str(rand_col))
+
         self.place_player(player, rand_row, rand_col)
 
-    def take_turn(self, player):
-        # TODO: Simply call the take_manual_turn function
+    # Calls the other methods to allow the game to be played.
+    def take_turn(self, player, game_mode, depth):
+
         print(str(player) + "'s Turn")
         if player == "X":
             self.take_manual_turn(player)
-        else:
-            self.take_minimax_turn(player)
+        elif (game_mode == "C"):
+            self.take_minimax_turn(player, depth)
+        elif (game_mode == "F"):
+            self.take_manual_turn("O")
 
-    def take_minimax_turn(self, player):
-        score, row, col = self.minimax(player)
+    # Wrapper function of the minimax AI.
+    def take_minimax_turn(self, player, depth):
+        score, row, col = self.minimax(player, depth)
+        #if (self.is_valid_move(row, col)):
         self.place_player(player, row, col)
 
-    # Used you pseudo-code to write the minimax function
-    # I was writing a different version of it, without using you
-    # structure as much but I'm sick so I couldn't really debug it
-    # and just decided to take the easy route. Sorry
-    def minimax(self, player):
+    # Implementation of the minimax AI.
+    def minimax(self, player, depth):
+        # Used you pseudo-code to write the minimax function
+        # I was writing a different version of it, without using you
+        # structure as much but I'm sick so I couldn't really debug it
+        # and just decided to take the easy route. Sorry
         if (self.check_win("O")):
             return 10, None, None
         elif (self.check_win("X")):
             return -10, None, None
-        elif(self.check_tie()):
+        elif(self.check_tie() or depth == 0):
             return 0, None, None
 
         opt_row, opt_col = -1, -1
@@ -96,7 +114,7 @@ class TicTacToe:
                     # [self.place_player(player, row, col); score = self.minimax("X")[0]; self.place_player("-", row, col) if self.board[row][col] == "-" else ( best, opt_row, opt_col = score, row, col if best < score )]
                     if self.board[row][col] == "-":
                         self.place_player(player, row, col)
-                        score = self.minimax("X")[0]
+                        score = self.minimax("X", depth - 1)[0]
                         self.place_player("-", row, col)
                         if best < score:
                             best, opt_row, opt_col = score, row, col
@@ -108,7 +126,7 @@ class TicTacToe:
                 for col in range(len(self.board[0])):
                     if self.board[row][col] == "-":
                         self.place_player(player, row, col)
-                        score = self.minimax("O")[0]
+                        score = self.minimax("O", depth - 1)[0]
                         self.place_player("-", row, col)
                         if worst > score:
                             worst, opt_row, opt_col = score, row, col
@@ -139,6 +157,7 @@ class TicTacToe:
         return score, row, col
         '''
 
+    # Checks to see if there are three in a row in a column.
     def check_col_win(self, player):
         # TODO: Check col win
         if self.board[0][0] == player and self.board[1][0] == player and self.board[2][0] == player:
@@ -149,6 +168,7 @@ class TicTacToe:
             return True
         return False
 
+    # Checks to see if there are three in a row in a row.
     def check_row_win(self, player):
         # TODO: Check row win
         if self.board[0][0] == player and self.board[0][1] == player and self.board[0][2] == player:
@@ -159,6 +179,7 @@ class TicTacToe:
             return True
         return False
 
+    # Checks to see if there are three in a row in a diagonal.
     def check_diag_win(self, player):
         # TODO: Check diagonal win
         if self.board[0][0] == player and self.board[1][1] == player and self.board[2][2] == player:
@@ -167,10 +188,12 @@ class TicTacToe:
             return True
         return False
 
+    # Checks to see if a player has won
     def check_win(self, player):
-        # TODO: Check win
+        # Check win
         return self.check_row_win(player) or self.check_col_win(player) or self.check_diag_win(player)
 
+    # Checks to see if there has been a draw.
     def check_tie(self):
         # TODO: Check tie
         for row in range(len(self.board)):
@@ -179,19 +202,32 @@ class TicTacToe:
                     return False
         return True
 
+    # Game is run through here. All other methods are called.
     def play_game(self):
         # TODO: Play game
         game = True
         player = "X"
-        self.print_instructions()
+        mode = self.print_instructions()
+
+        # The line below is stupid because at any depth below 6
+        # the AI essentially plays like the random bot. However, I feel like
+        # I could find a way to add levels of difficulty through this.
+        # depth = random.randint(3, 9)
+        if (mode == 'C'):
+            # The upper limit of 8 in unnecessary but it seems cooler for it
+            # to be able to go up to level 5. At any point above 6 it becomes
+            # unbeatable I think.
+            depth = random.randint(4, 8)
+            print("Level of AI: " + str(depth - 3))
+
         self.print_board()
         while game:
-            self.take_turn(player)
+            self.take_turn(player, mode, depth)
             if self.check_win(player):
-                print(str(player) + " wins!")
+                print(str(player) + " wins")
                 game = False
             elif self.check_tie():
-                print("Tie game!")
+                print("Tie game")
                 game = False
             else:
                 if player == "X":
